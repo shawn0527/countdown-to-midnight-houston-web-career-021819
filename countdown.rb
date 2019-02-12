e773922df746651fe40867c94f639e02fdb7f35c
Shawn Ren <doggy-ren@hotmail.com>
2019-02-12 16:35:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  if n == 12
+    return "HAPPY NEW YEAR!"
+  else
+    while n > 0 do
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+  end
+end
+
+
+def countdown_with_sleep(num_sec)
+  sleep(num_sec)
 end
