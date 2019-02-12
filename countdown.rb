25d2fd180c6d3512c6ac84cb9e6be401ca60d757
Shawn Ren <doggy-ren@hotmail.com>
2019-02-12 16:32:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  if n == 12
+    return "HAPPY NEW YEAR!"
+  else
+    while n > 0 do
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+  end
 end
